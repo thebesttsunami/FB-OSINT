85a487c99fa14634a77b31145853f6d56551e91f
The Best Tsunami <162686236+thebesttsunami@users.noreply.github.com>
2025-01-03 21:56:50 +0700

    Create CONTOH-OSINT.sh

---
+++ CONTOH-OSINT.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+#[created 23: 55 - 12/12/2024]#
+
+echo "[uploading...] DATA BASE - :::: :::: [[SMPN 101 JAKARTA]]"
+        echo "NAMA: RISKY xxxxxx PRATAMA"
+      echo "USIA: 15 thn"
+    echo "KELAS: 8.5 (2 SMP)"
+  echo "IBU: Dr. xxxxxx wati Sp,d S.sos"
+    echo "AYAH: Dr. Risky xxxxxxx S.H M.Hum"
+      echo "NIS: 52xxx"
+        echo "NISN: 527xxxx"
+           echo "TLP: 628 5194xx xxxx"
+            echo "ALAMAT: Penjaringan Jakata Utara, Indonesia"
+          echo "[[valid - :::: :::: database...]]"
