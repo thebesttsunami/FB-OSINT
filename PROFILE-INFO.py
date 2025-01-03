0e4cd523373d86f7ca6cbf8d5d9828c330e047ca
The Best Tsunami <162686236+thebesttsunami@users.noreply.github.com>
2025-01-03 21:39:19 +0700

    Create PROFILE-INFO.py

---
+++ PROFILE-INFO.py
@@ -0,0 +1,15 @@
+#!/bin/python
+
+import facebook
+
+Masukkan API Key dan ID Profil
+api_key = "882a8490361da98702bf97a021ddc14d"
+id_profil = "// ID TARGET //"
+
+Buat objek Facebook
+graph = facebook.GraphAPI(api_key)
+
+Ambil informasi profil
+profil = graph.get_object(id=id_profil, fields='name,email,birthday,location')
+
+print(profil)
