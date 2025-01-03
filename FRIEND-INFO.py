fd02af38bb9d73710a8c761aa023ebdae864b10a
The Best Tsunami <162686236+thebesttsunami@users.noreply.github.com>
2025-01-03 21:42:14 +0700

    Create FRIEND-INFO.py

---
+++ FRIEND-INFO.py
@@ -0,0 +1,13 @@
+import facebook
+
+Masukkan API Key dan ID Profil
+api_key = "882a8490361da98702bf97a021ddc14d"
+id_profil = "// ID TARGET //"
+
+Buat objek Facebook
+graph = facebook.GraphAPI(api_key)
+
+Ambil daftar teman
+teman = graph.get_connections(id=id_profil, connection_name='friends')
+
+print(teman)
