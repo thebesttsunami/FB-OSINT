c1c260b9fc54d64f67d67f373fef2ad342ae1a5b
The Best Tsunami <162686236+thebesttsunami@users.noreply.github.com>
2025-01-05 22:39:09 +0700

    Update PROFILE-OSINT.py

--- PROFILE-OSINT.py
+++ PROFILE-OSINT.py
@@ -1,35 +1,31 @@
+#!/usr/bin/python
+
 import facebook
 import requests
 from bs4 import BeautifulSoup
 
-Masukkan API Key dan ID Profil Target
 api_key = "882a8490361da98702bf97a021ddc14d"
 id_profil = "100087020197894"
 
-Buat objek Facebook
 graph = facebook.GraphAPI(api_key)
 
-Mengumpulkan Informasi Profil
 def get_profil_info():
     profil = graph.get_object(id=id_profil, fields='name,birthday,location,hometown,age_range')
     return profil
 
-Mengumpulkan Daftar Teman
+
 def get_teman():
     teman = graph.get_connections(id=id_profil, connection_name='friends')
     return teman
 
-Mengumpulkan Grup yang Diikuti
 def get_grup():
     grup = graph.get_connections(id=id_profil, connection_name='groups')
     return grup
 
-Mengumpulkan Informasi Akun
 def get_akun_info():
     akun = graph.get_object(id=id_profil, fields='created_time')
     return akun
-
-Menjalankan Kode
+    
 if __name__ == "__main__":
     profil_info = get_profil_info()
     teman = get_teman()
